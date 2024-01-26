b9323129cc7e59af8668a237551d95da7f2d32f2
jaykido <46677280+jaykido@users.noreply.github.com>
2024-01-26 20:46:48 +0300

    first pull request

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import styles from "./singlePage.module.css";
 
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/post.png" alt="" fill />
+      </div>
+      <div className={styles.textContainer}>SinglePostPage</div>
+    </div>
+  );
 };
-
 export default SinglePostPage;
