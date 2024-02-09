1c2ecc7d2a6c4710aeaf7df4d54e768361cb1be5
jaykido <46677280+jaykido@users.noreply.github.com>
2024-02-09 20:24:52 +0300

    typo correction

--- src/components/registerForm/registerForm.js
+++ src/components/registerForm/registerForm.js
@@ -1,6 +1,6 @@
 "use client";
 import { register } from "@/lib/actions";
-import styles from "./RegisterForm.module.css";
+import styles from "./registerForm.module.css";
 import { useFormState } from "react-dom";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
