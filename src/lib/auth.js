34cf82ecfb671f3792f89ab5a9cc1e51da269245
jaykido <46677280+jaykido@users.noreply.github.com>
2024-02-10 13:11:00 +0300

    updates for vercel

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,6 +4,7 @@ import credentialsProvider from "next-auth/providers/credentials";
 import { connectToDb } from "./utils";
 import { User } from "./models";
 import bcrypt from "bcryptjs";
+import { authConfig } from "./auth.config";
 
 const login = async (credentials) => {
   try {
@@ -35,6 +36,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  ...authConfig,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
@@ -73,6 +75,6 @@ export const {
       }
       return true;
     },
-    authorized({ auth, requrst }) {},
+    ...authConfig.callbacks,
   },
 });
